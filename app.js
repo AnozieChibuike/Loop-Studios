73a743a747ba25b7cd9d558ceedbe1818433526c
Miracle <miraclepatidehen@gmail.com>
2023-03-15 04:45:49 +0100

    Init project

---
+++ app.js
@@ -0,0 +1,31 @@
+const navBtn = document.querySelector('.nav-btn')
+const sidebar = document.querySelector('.sidebar')
+
+// toogle sidebar
+navBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar')
+})
+
+sidebar.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar')
+})
+
+// scroll animations
+ScrollReveal().reveal('.hero', {
+  origin: 'left',
+  distance: '40px',
+  delay: 1000,
+  easing: 'ease-in',
+})
+ScrollReveal().reveal('img', {
+  origin: 'bottom',
+  distance: '40px',
+  delay: 300,
+  easing: 'ease-in',
+})
+ScrollReveal().reveal('article', {
+  origin: 'right',
+  distance: '40px',
+  delay: 300,
+  easing: 'ease-in',
+})
