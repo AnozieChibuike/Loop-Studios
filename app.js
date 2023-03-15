9b68b12e473c946c4486242338812e130f51d5e5
Miracle <miraclepatidehen@gmail.com>
2023-03-15 06:29:50 +0100

    updated js

--- app.js
+++ app.js
@@ -17,12 +17,6 @@ ScrollReveal().reveal('.hero', {
   delay: 1000,
   easing: 'ease-in',
 })
-ScrollReveal().reveal('.sidebar', {
-  origin: 'left',
-  distance: '40px',
-  delay: 200,
-  easing: 'ease-in',
-})
 ScrollReveal().reveal('img', {
   origin: 'bottom',
   distance: '40px',
